005fd49a6e98b672b2a03bf6dd3b95501336efc9
Calvin Firlansyah <muhammadlinggar9@gmail.com>
2023-05-15 22:24:34 -0700

    Update TindeUnblur.user.js

--- TindeUnblur.user.js
+++ TindeUnblur.user.js
@@ -1,39 +1,51 @@
-// Ini adalah contoh skrip JavaScript yang menggunakan token dari localStorage
+// ==UserScript==
+// @name         Tinder API Script
+// @namespace    your-namespace
+// @version      1.0
+// @description  Script to get clean photos of users who liked you on Tinder API
+// @author       Your Name
+// @match        https://example.com/*  // Ganti dengan URL Tinder atau halaman yang sesuai
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
 
-// Mengimpor modul 'axios' untuk melakukan permintaan HTTP
-const axios = require('axios');
+(function() {
+    'use strict';
 
-// Mengambil token dari localStorage
-const accessToken = localStorage.getItem('TinderWeb/APIToken');
+    // Mengambil token dari localStorage
+    const accessToken = localStorage.getItem('TinderWeb/APIToken');
 
-// Memastikan token tersedia sebelum melanjutkan
-if (!accessToken) {
-  console.log('Token tidak ditemukan di localStorage');
-  return;
-}
+    // Memastikan token tersedia sebelum melanjutkan
+    if (!accessToken) {
+        console.log('Token tidak ditemukan di localStorage');
+        return;
+    }
 
-// Mengambil daftar pengguna yang menyukai Anda
-axios.get('https://api.gotinder.com/v2/fast-match/preview', {
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-})
-  .then(response => {
-    const likedUsers = response.data.data.results;
-    
-    // Mengambil foto-foto pengguna yang menyukai Anda
-    likedUsers.forEach(user => {
-      const photos = user.photos;
+    // Mengambil daftar pengguna yang menyukai Anda
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'https://api.gotinder.com/v2/fast-match/preview',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        },
+        onload: function(response) {
+            const likedUsers = JSON.parse(response.responseText).data.results;
 
-      // Mengambil foto bersih dari setiap pengguna
-      photos.forEach(photo => {
-        if (photo.processedFiles && photo.processedFiles.length > 0) {
-          const cleanPhotoUrl = photo.processedFiles[0].url;
-          console.log(cleanPhotoUrl);
+            // Mengambil foto-foto pengguna yang menyukai Anda
+            likedUsers.forEach(user => {
+                const photos = user.photos;
+
+                // Mengambil foto bersih dari setiap pengguna
+                photos.forEach(photo => {
+                    if (photo.processedFiles && photo.processedFiles.length > 0) {
+                        const cleanPhotoUrl = photo.processedFiles[0].url;
+                        console.log(cleanPhotoUrl);
+                    }
+                });
+            });
+        },
+        onerror: function(error) {
+            console.log('Terjadi kesalahan saat mengambil pengguna yang menyukai Anda:', error);
         }
-      });
     });
-  })
-  .catch(error => {
-    console.log('Terjadi kesalahan saat mengambil pengguna yang menyukai Anda:', error);
-  });
+
+})();
