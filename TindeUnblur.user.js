87938781e6de91fff34e290d2deade1fd342f7a4
Calvin Firlansyah <muhammadlinggar9@gmail.com>
2023-05-15 22:46:36 -0700

    Update TindeUnblur.user.js

--- TindeUnblur.user.js
+++ TindeUnblur.user.js
@@ -1,51 +1,100 @@
 // ==UserScript==
-// @name         Tinder API Script
-// @namespace    Unknown240
-// @version      1.0
-// @description  Script to get clean photos of users who liked you on Tinder API
-// @author       Your Name
-// @match        https://tinder.com/
-// @grant        GM_xmlhttpRequest
+// @name        Tinder Deblur
+// @namespace   Violentmonkey Scripts
+// @match       https://tinder.com/*
+// @grant       none
+// @version     2.6
+// @author      Tajnymag
+// @description Simple script using the official Tinder API to get clean photos of the users who liked you
 // ==/UserScript==
-
-(function() {
-    'use strict';
-
-    // Mengambil token dari localStorage
-    const accessToken = localStorage.getItem('TinderWeb/APIToken');
-
-    // Memastikan token tersedia sebelum melanjutkan
-    if (!accessToken) {
-        console.log('Token tidak ditemukan di localStorage');
-        return;
-    }
-
-    // Mengambil daftar pengguna yang menyukai Anda
-    GM_xmlhttpRequest({
-        method: 'GET',
-        url: 'https://api.gotinder.com/v2/fast-match/preview',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        },
-        onload: function(response) {
-            const likedUsers = JSON.parse(response.responseText).data.results;
-
-            // Mengambil foto-foto pengguna yang menyukai Anda
-            likedUsers.forEach(user => {
-                const photos = user.photos;
-
-                // Mengambil foto bersih dari setiap pengguna
-                photos.forEach(photo => {
-                    if (photo.processedFiles && photo.processedFiles.length > 0) {
-                        const cleanPhotoUrl = photo.processedFiles[0].url;
-                        console.log(cleanPhotoUrl);
-                    }
-                });
-            });
-        },
-        onerror: function(error) {
-            console.log('Terjadi kesalahan saat mengambil pengguna yang menyukai Anda:', error);
-        }
-    });
-
-})();
+ 
+/**
+ * Core logic of the script
+ */
+async function unblur() {
+	const teasers = await fetch('https://api.gotinder.com/v2/fast-match/teasers', {
+		headers: {
+			'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
+			platform: 'android'
+		},
+	})
+		.then((res) => res.json())
+		.then((res) => res.data.results);
+	const teaserEls = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
+ 
+	for (let i = 0; i < teaserEls.length; ++i) {
+		const teaser = teasers[i];
+		const teaserEl = teaserEls[i];
+ 
+		const teaserImage = `https://preview.gotinder.com/${teaser.user._id}/original_${teaser.user.photos[0].id}.jpeg`;
+ 
+		teaserEl.style.backgroundImage = `url(${teaserImage})`;
+	}
+}
+ 
+/**
+ * Awaits the first event of the specified listener
+ * @param {EventTarget} target
+ * @param {string} eventType
+ * @returns {Promise<void>}
+ */
+async function once(target, eventType) {
+	return new Promise((resolve) => {
+		target.addEventListener(eventType, () => {
+			target.removeEventListener(eventType, resolve);
+			resolve();
+		});
+	});
+}
+ 
+/**
+ * Awaits until the main app element is found in DOM and returns it
+ * @returns {Promise<HTMLElement>}
+ */
+async function waitForApp() {
+	const getAppEl = (parent) => parent.querySelector('.App');
+	let appEl = getAppEl(document.body);
+ 
+	if (appEl) return appEl;
+ 
+	return new Promise((resolve) => {
+		new MutationObserver((_, me) => {
+			appEl = getAppEl(document.body);
+			if (appEl) {
+				me.disconnect();
+				resolve(appEl);
+			}
+		}).observe(document.body, { subtree: true, childList: true });
+	});
+}
+ 
+async function main() {
+	// check if running as a userscript
+	if (typeof GM_info === 'undefined') {
+		console.warn(
+			'[TINDER DEBLUR]: The only supported way of running this script is through a userscript management browser addons like Violentmonkey, Tampermonkey or Greasemonkey!'
+		);
+		console.warn(
+			'[TINDER DEBLUR]: Script was not terminated, but you should really look into the correct way of running it.'
+		);
+	}
+ 
+	// wait for a full page load
+	await once(window, 'load');
+	const appEl = await waitForApp();
+ 
+	const pageCheckCallback = () => {
+		if (['/app/likes-you', '/app/gold-home'].includes(location.pathname)) {
+			console.debug('[TINDER DEBLUR]: Deblurring likes');
+			unblur();
+		}
+	};
+ 
+	// setup navigation observer
+	const observer = new MutationObserver(pageCheckCallback);
+	observer.observe(appEl, { subtree: true, childList: true });
+ 
+	// setup loop based observer (every 5s)
+	setInterval(pageCheckCallback, 5000);
+}
+main().catch(console.error);
